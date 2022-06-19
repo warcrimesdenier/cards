6271d850c1fde3d5e96ea6c42eb87e39b40eedee
warcrimesdenier <107793496+warcrimesdenier@users.noreply.github.com>
2022-06-19 08:44:56 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	DEBUG: true,
 	REDIS_PORT: 'redis://:p93caa2d35d8d227f541368bbc2f2346168024e35982499e4300f1767c6582289@ec2-54-209-29-252.compute-1.amazonaws.com:12659',
-	LISTEN_PORT:  443,//process.env.PORT,
+	LISTEN_PORT: process.env.PORT,
 	TRUST_X_FORWARDED_FOR: true,
 	SOCKJS_PREFIX: '/sockjs',
 	SOCKJS_URL: 'http://localhost:8000/sockjs',
