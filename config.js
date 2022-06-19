2c34838a3e78c6b3c76ba7eef65b4cf0e38e1c55
warcrimesdenier <107793496+warcrimesdenier@users.noreply.github.com>
2022-06-19 05:08:17 -0700

    changed port settings

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	DEBUG: false,
-	REDIS_PORT: 6379,
+	REDIS_PORT: 'redis://:p93caa2d35d8d227f541368bbc2f2346168024e35982499e4300f1767c6582289@ec2-54-209-29-252.compute-1.amazonaws.com:12659',
 	LISTEN_PORT: 8080,
 	TRUST_X_FORWARDED_FOR: false,
 	SOCKJS_PREFIX: '/sockjs',
