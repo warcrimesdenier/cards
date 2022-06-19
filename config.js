4278e9fd46006df4b97dddfc3e6219ce48960124
warcrimesdenier <107793496+warcrimesdenier@users.noreply.github.com>
2022-06-19 05:12:21 -0700

    changed listeing port

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	DEBUG: false,
 	REDIS_PORT: 'redis://:p93caa2d35d8d227f541368bbc2f2346168024e35982499e4300f1767c6582289@ec2-54-209-29-252.compute-1.amazonaws.com:12659',
-	LISTEN_PORT: 8080,
+	LISTEN_PORT: 80,
 	TRUST_X_FORWARDED_FOR: false,
 	SOCKJS_PREFIX: '/sockjs',
 	SOCKJS_URL: 'http://localhost:8000/sockjs',
