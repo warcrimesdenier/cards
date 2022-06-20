53ce77268a5d67d71f4df8b784d36d344e877a97
warcrimesdenier <107793496+warcrimesdenier@users.noreply.github.com>
2022-06-19 17:10:10 -0700

    maek sock relative

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ module.exports = {
 	REDIS_PORT: 'redis://:p93caa2d35d8d227f541368bbc2f2346168024e35982499e4300f1767c6582289@ec2-54-209-29-252.compute-1.amazonaws.com:12659',
 	LISTEN_PORT: process.env.PORT,
 	TRUST_X_FORWARDED_FOR: true,
-	SOCKJS_PREFIX: '/sockjs',
-	SOCKJS_URL: 'https://localhost:80/sockjs',
-	SOCKJS_SCRIPT_URL: 'http://localhost:80/sockjs-1.1.1.min.js',//'https://cdn.jsdelivr.net/npm/sockjs-client@1/dist/sockjs.min.js', 
+	//SOCKJS_PREFIX: '/sockjs',
+	SOCKJS_URL: '/sockjs',//'https://localhost:80/sockjs',
+	SOCKJS_SCRIPT_URL: '/sockjs-0.3.2.min.js',//'https://cdn.jsdelivr.net/npm/sockjs-client@1/dist/sockjs.min.js', 
 	GAME_ID: 1,
 	NAME_EXPIRY: 60 * 60 * 24 * 14,
 };
